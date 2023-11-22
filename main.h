3d07cf9cec417e2604ca9d99593c38b55623a400
haoningng <nick.nghn@gmail.com>
2023-11-21 16:59:35 -0800

    add main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+ * struct print_type - Struct print_type
+ *
+ * @type: the character representating different format type
+ * @f: the function pointer to print different format type
+ */
+typedef struct print_type
+{
+	char type;
+	void (*f)(va_list arg);
+} print_type;
+
+int _putchar(char c);
+void print_char(va_list arg);
+void print_string(va_list arg);
+void print_percent(va_list arg);
+void print_int(va_list arg);
+void print_decimal(va_list arg);
+int _printf(const char *format, ...);
+
+#endif
