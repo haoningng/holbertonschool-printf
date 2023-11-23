1af1e729b39f4df38998a5cb178d503a7acb0dfa
root <7688@holbertonschool.com>
2023-11-22 16:44:59 -0800

    40lines

--- printf.c
+++ printf.c
@@ -5,6 +5,16 @@
 #include <unistd.h>
 #include "main.h"
 
+/* Declare a struct array for different print format  */ 
+print_type print_format[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'i', print_int},
+	{'d', print_int},
+	{'%', print_percent},
+	{'\0', NULL}
+};
+
 /**
  * _printf - produces output according to a format.
  * @format: string to be printed
@@ -16,25 +26,13 @@ int _printf(const char *format, ...)
 	int i, j, len, printed;
 	va_list arg;
 
-	/* Declare a struct array for different print format  */
-	print_type print_format[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'i', print_int},
-		{'d', print_int},
-		{'%', print_percent},
-		{'\0', NULL}
-	};
-	/*add a check for a 'NULL' format string*/
 	if (format == NULL)
 	{
 		return (-1);
 	}
 
 	va_start(arg, format);
-	i = 0;
-	len = 0;
-	/* Check if format is NULL and whether we have reached '\0' */
+	i = len = 0;
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] != '%')
@@ -45,16 +43,13 @@ int _printf(const char *format, ...)
 		else if (format[i] == '%')
 		{
 			j = 0;
-			/* Check if we have reached '\0' in print_format.type */
 			while (print_format[j].type != '\0')
 			{
-				/* if there is nothing after % sign, print nothing */
 				if (format[i + 1] == '\0')
 				{
 					va_end(arg);
 					return (-1);
 				}
-				/* if we found a match */
 				else if (print_format[j].type == format[i + 1])
 				{
 					printed = print_format[j].f(arg);
@@ -62,7 +57,6 @@ int _printf(const char *format, ...)
 					i++;
 					break;
 				}
-				/* if no matching specifier, print % sign */
 				else if (format[i + 1] != 'c' && format[i + 1] != 's' && format[i + 1] != 'd' && format[i +  1] != 'i' && format[i + 1] != '%')
 				{
 					_putchar('%');
